9f6a4cad57d80ca53ad0eaf815717565f914ddcb
Alvaro Rodriguez Rubio <alvaro.rodriguez.rubio@estudiantat.upc.edu>
2024-02-27 11:56:35 +0100

    all files

---
+++ Textures/Levels/TerrainEntrega1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TerrainEntrega1" tilewidth="16" tileheight="16" tilecount="85" columns="17">
+ <image source="Terrain_1.png" width="272" height="80"/>
+</tileset>
---
+++ Textures/Terrain1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Terrain1" tilewidth="16" tileheight="16" tilecount="85" columns="17">
+ <image source="Levels/Terrain_1.png" width="272" height="80"/>
+</tileset>
